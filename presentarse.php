10ad56459d4eab636f8fa52d7a884710737b6344
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-28 23:56:34 -0400

    Update presentarse.php

--- presentarse.php
+++ presentarse.php
@@ -1,36 +1,37 @@
 <?php 
 
 ?>
+<link rel="stylesheet" type="text/css" href="css/estilofinal.css">
 	<section id="main-content">
 		<article>
 			<div class="content">
-				<h1><center>Lista de requisitos para presentarse al Control Nutricional</center></h1><br>
+				<h1><center>LISTA DE REQUISITOS PARA PRESENTARSE AL CONTROL NUTRICIONAL</center></h1><br>
 			<table border="1">
 				<tr>		
-				<td align="center"><font size="5">Requisitos</font> </td>
-				<td align="center"><font size="5">Valoracion nutricional</font> </td>
+				<td ><font size="5">Requisitos</font> </td>
+				<td ><font size="5">Valoracion nutricional</font> </td>
 				</tr>
 				<tr>
-				<td height="100"><font size="4">-> 1. Presentarse de acuerdo al cronograma establecido.</font></td>
+				<td height="100"><font size="4"> 1. Presentarse de acuerdo al cronograma establecido.</font></td>
 				<td><img src="img/calendario.png" height="150" width="200"></td>
 				</tr>
 				<tr>
-				<td height="100"><font size="4">-> 2. Presentarse en las horas de atencion establecidas</font></td>
+				<td height="100"><font size="4"> 2. Presentarse en las horas de atencion establecidas</font></td>
 				<td><img src="img/horario.jpg" height="150" width="200"></td>
 				</tr>
 				<tr>
-				<td height="100"><font size="4">-> 3. Traer Carnet de Identidad original.</font></td>
+				<td height="100"><font size="4"> 3. Traer Carnet de Identidad original.</font></td>
 				<td><img src="img/ci.jpg" height="150" width="200"></td></tr>
 				<tr>
-				<td height="100"><font size="4">-> 4. Traer Matricula Universitaria.</font></td>
+				<td height="100"><font size="4"> 4. Traer Matricula Universitaria.</font></td>
 				<td><img src="img/matricula.jpg" height="150" width="200"></td>
 				</tr>
 				<tr>
-				<td height="100"><font size="4">-> 5. Presentarse con excelente higiene personal.</font></td>
+				<td height="100"><font size="4"> 5. Presentarse con excelente higiene personal.</font></td>
 				<td><img src="img/higiene.jpg" height="150" width="200"></td>
 				</tr>
 				<tr>
-				<td height="100"><font size="4">-> 6. Traer su corto y polera.</font></td>
+				<td height="100"><font size="4"> 6. Traer su corto y polera.</font></td>
 				<td><img src="img/corto.jpg" height="150" width="200"></td>
 				</tr>
 			</table>
