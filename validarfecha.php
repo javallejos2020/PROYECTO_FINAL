b669d06a23cca00e4c1b814d880b2d1a815d7c31
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-28 23:57:02 -0400

    Update validarfecha.php

--- validarfecha.php
+++ validarfecha.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 if (isset($_POST["Verifica"])):
 	$facu=$_POST["facultad"];
 	$fecha=$_POST["fecha"];
-	$mensaje='';
+	//$mensaje='';
 	$sql="SELECT * FROM cronograma WHERE facultad like '$facu' and('$fecha' >=fecha_inicio AND '$fecha' <= fecha_fin)";
 	$result=mysqli_query($con,$sql);
 	echo '<table class="datos">';
@@ -18,17 +18,18 @@ if (isset($_POST["Verifica"])):
 				echo'<tr><td colspan="3">'.$mensaje.'</td></tr>';
 				echo '<tr><td colspan="3">'.$fecha.'</td></tr>';
 				$mensaje='SI';
+				
 			}
 			if(empty($mensaje)){
 				echo'<tr><td>NO SE ENCUENTRA EN EL RANGO</td></tr>';
 				$mensaje='NO';
+				
 			}
 	echo '</table>';
 
 endif ?>
-<p>Campo que se llena una vez verificado</p>
 <br>
-<input type="text" name="cond" value='<?php if(!empty($mensaje))echo $mensaje; ?>'>
+<input type="text" name="cond" placeholder="Verificación Automática" value='<?php if(!empty($mensaje))echo $mensaje; ?>'>
 <br><br>
 
 
