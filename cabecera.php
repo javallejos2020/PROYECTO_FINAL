1bcde244007465c7f047db8d0125b7b3ae100bc6
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-28 20:03:34 -0400

    Create cabecera.php

---
+++ cabecera.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Cronograma</title>
+        <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header id="main-header">
+		<a id="logo-header" href="#">
+			<img src="img/logoumsa.png" width="145">
+		</a> <!-- / #logo-header -->
+		<nav>
+			<ul>
+				<span class="site-name">UMSA - Universad Mayor de San Andrés</span>
+			</ul>
+		</nav><!-- / nav -->
+	</header><!-- / #main-header -->
