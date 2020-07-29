ddbb93a45fd11edeb8e8fcba57de5a22ce25f1f6
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-28 23:55:51 -0400

    Update historial.php

--- historial.php
+++ historial.php
@@ -1,6 +1,7 @@
 <br>
 <?php 
   if (isset($_POST['ver'])) {?>
+    
     <table border="1" cellspacing=1 cellpadding=2 style="font-size: 8pt">
         <tr>
           <td align="center"><font face="crono" size="4"><b>CI</b></font></td>
@@ -10,10 +11,19 @@
           <td align="center"><font face="crono" size="4"><b>TALLA</b></font></td>
           <td align="center"><font face="crono" size="4"><b>ESTADO</b></font></td>
           </tr>
+		  
     <?php $ci=$_POST['ci'];
     //echo $ci."MMMMMM";
     $sql="select ci_estudiante, nombre, apellidos, peso, talla, estado from historial where ci_estudiante='$ci'";
-    //echo $sql."_______";
+	$sql2 = "select apellidos, nombre from estudiante where ci_estudiante like '$ci'";
+	$query = mysqli_query($con,$sql2);
+	while ($mostrar=mysqli_fetch_array($query)) {
+			?>	
+		<h2><?php echo  "Estudiante: ".$mostrar['apellidos']." ".$mostrar['nombre']?></h2><br>
+
+		<?php
+	}
+	
     $result=mysqli_query($con, $sql);
         //echo $result."--------";
           while($row = mysqli_fetch_array($result))
