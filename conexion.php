4518b911e20400a4b5f84609d50f4bdd1cd985bc
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-28 23:55:08 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
-$con=mysqli_connect("localhost","javallejos","123456");
+$con=mysqli_connect("localhost:33066","anachui","123456");
 mysqli_select_db($con,"academico_4");
 if(!$con){
 	die("No hay conexion:".mysql_connect_error());
 }
-$conn=mysqli_connect("localhost","root","");
+$conn=mysqli_connect("localhost:33066","anachui","123456");
 mysqli_select_db($conn,"flujo2");
 if(!$conn){
 	die("No hay conexion:".mysql_connect_error());
